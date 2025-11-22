08a1ec11259cbeb8276f7cc5eb4dcfbed54f9e30
ranim <laifiranim.lr@gmail.com>
2025-11-22 22:03:47 +0100

    Fix Flask routes and Render port

--- app.py
+++ app.py
@@ -20,6 +20,6 @@ def secret():
         return jsonify({"message": "Accès refusé !"}), 401
 
 if __name__ == "__main__":
-    port=int(os.environ.get("PORT",5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
 
