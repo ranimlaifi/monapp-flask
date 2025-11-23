de717dd6d9d1358dd85a34b7d1f70cd71848802c
ranim <laifiranim.lr@gmail.com>
2025-11-23 16:51:04 +0100

    Ajout dashboard + CSS + pages HTML

--- app.py
+++ app.py
@@ -1,41 +1,37 @@
 from flask import Flask, request, jsonify, render_template
-from dotenv import load_dotenv
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 import os
 
 app = Flask(__name__)
 
-load_dotenv()
-API_KEY = os.getenv("API_KEY")
+API_KEY = os.getenv("API_KEY", "123456")
 
-# Rate limiting
-limiter = Limiter(
-    get_remote_address,
-    app=app,
-    default_limits=["5 per minute"]
-)
+limiter = Limiter(get_remote_address, app=app, default_limits=["10 per minute"])
+
+# Comptage simple en mémoire (juste pour démo)
+stats = {"200": 0, "403": 0, "404": 0}
 
 @app.route('/')
 def home():
     return render_template("index.html")
-
-@app.route('/dashboard')
-def dashboard():
-    return render_template("dashboard.html")
 
 @app.route('/secret')
 @limiter.limit("3 per minute")
 def secret():
     key = request.headers.get('X-API-KEY')
     if key == API_KEY:
+        stats["200"] += 1
         return jsonify({"message": "Accès autorisé !"})
     else:
+        stats["403"] += 1
         return jsonify({"message": "Accès refusé !"}), 403
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html", stats=stats)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
 
 
